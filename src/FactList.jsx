389db9fa562ba845947d46c71ac3c78eef9def53
Muhammad Zohaib Ul Hassan <zohaibfas@gmail.com>
2023-02-22 14:21:56 +0500

    update factlist

--- src/FactList.jsx
+++ src/FactList.jsx
@@ -3,6 +3,21 @@ import { CATEGORIES } from "./catagories";
 import supabase from "./supabase";
 const FactList = ({ facts, setFacts }) => {
   console.log(facts);
+
+  async function votesHandler(fact, factstring) {
+    console.log(fact.id, factstring);
+    const { data: updatedFact, error } = await supabase
+      .from("facts")
+      .update({ [factstring]: fact[factstring] + 1 })
+      .eq("id", fact.id)
+      .select();
+
+    if (!error)
+      setFacts((facts) =>
+        facts.map((f) => (f.id === fact.id ? updatedFact[0] : f))
+      );
+  }
+
   return (
     <>
       <section className="section">
@@ -27,24 +42,29 @@ const FactList = ({ facts, setFacts }) => {
               </span>
               <div className="vote-buttons">
                 <button
-                  onClick={async () => {
-                    const { data, error } = await supabase
-                      .from("facts")
-                      .update({ votesIntresting: fact.votesIntresting + 1 })
-                      .eq("id", fact.id)
-                      .select();
-                    console.log(data);
-                    if (!error) {
-                      setFacts((facts) =>
-                        facts.map((f) => (f.id === fact.id ? data[0] : f))
-                      );
-                    }
-                  }}
+                  // onClick={async () => {
+                  //   const { data, error } = await supabase
+                  //     .from("facts")
+                  //     .update({ votesIntresting: fact.votesIntresting + 1 })
+                  //     .eq("id", fact.id)
+                  //     .select();
+                  //   console.log(data);
+                  //   if (!error) {
+                  //     setFacts((facts) =>
+                  //       facts.map((f) => (f.id === fact.id ? data[0] : f))
+                  //     );
+                  //   }
+                  // }}
+                  onClick={() => votesHandler(fact, "votesIntresting")}
                 >
                   👍 {fact.votesIntresting}
                 </button>
-                <button>⛔️ {fact.votesFalse}</button>
-                <button>🤯 {fact.votesMindblowing}</button>
+                <button onClick={() => votesHandler(fact, "votesFalse")}>
+                  ⛔️ {fact.votesFalse}
+                </button>
+                <button onClick={() => votesHandler(fact, "votesMindblowing")}>
+                  🤯 {fact.votesMindblowing}
+                </button>
               </div>
             </li>
           ))}
